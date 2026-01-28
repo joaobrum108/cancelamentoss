4ba0bf649780185f495397fc9ed5efa58b8b9af3
joaobrum108 <joaobrum108gmail.com>
2026-01-28 17:23:58 -0300

    front att

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <v-container>
     <List />
+    <Dashboard />
   </v-container>
 </template>
 
 <script setup>
 import List from '../components/List.vue';
-
+import Dashboard from '../components/dashboard.vue';
 </script>
 
 <style scoped></style>
